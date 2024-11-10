e8336bb304a0b59a659b84c3cd3bc3f94c5c0396
Аглямов Айнур <aynur.aglyamov@domrf.ru>
2024-11-10 22:07:52 +0500

    HW4 Использование SpringShell и SpringBootTest

---
+++ hw04/src/main/java/ru/otus/hw04/config/LocaleConfig.java
@@ -0,0 +1,8 @@
+package ru.otus.hw04.config;
+
+import java.util.Locale;
+
+public interface LocaleConfig {
+    Locale getLocale();
+
+}
---
+++ hw04/src/main/java/ru/otus/hw04/service/TestServiceImpl.java
@@ -0,0 +1,54 @@
+package ru.otus.hw04.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.hw04.dao.QuestionDao;
+import ru.otus.hw04.domain.Answer;
+import ru.otus.hw04.domain.Student;
+import ru.otus.hw04.domain.TestResult;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final LocalizedIOService ioService;
+
+    private final QuestionDao questionDao;
+
+    @Override
+    public TestResult executeTestFor(Student student) {
+        ioService.printLineLocalized("TestService.answer.the.questions");
+        var questions = questionDao.findAll();
+        var testResult = new TestResult(student);
+
+        for (var question: questions) {
+            ioService.printLine(question.text());
+            List<Answer> answers = question.answers();
+            printAnswers(answers);
+            int userAnswer = ioService.readIntForRangeWithPromptLocalized(
+                    1,
+                    answers.size(),
+                    "TestService.prompt",
+                    "TestService.error"
+            );
+            ioService.printLine("");
+
+            var isAnswerValid = answers.get(userAnswer - 1).isCorrect();
+
+            testResult.applyAnswer(question, isAnswerValid);
+        }
+        return testResult;
+    }
+
+    private void printAnswers(List<Answer> answers) {
+        var optionCounter = new AtomicInteger(1);
+        answers.forEach(answer -> {
+            String optionPrefix = "\t" + optionCounter.getAndIncrement() + ") ";
+            ioService.print(optionPrefix);
+            ioService.printLine(answer.text());
+        });
+    }
+}
